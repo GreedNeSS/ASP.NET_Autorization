1489f24ed500766fe001d0ad63d189c1f57fdf30
GreedNeSS <ruslan199126@gmail.com>
2022-05-31 21:40:03 +0300

    Added introduction example.

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.AspNetCore.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/hello", [Authorize]() => "Hello World!");
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
